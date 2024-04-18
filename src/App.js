6289a1865e1a650140143762868f0b223efafe7b
Your Name <you@example.com>
2024-04-18 15:44:18 +0700

    m2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
+import {useState} from "react"
 
 function App() {
+
+  const [word,setWord] = useState('')
+
   return (
-    <div className="App">
-     app
+    <div className="flex h-screen justify-center items-center gap-3">
+     <input 
+     type="text"
+     className="outline-none border border-blue-500 px-4 py-2 rounded-md w-[400px]"
+     value={word}
+     onChange={(e)=>{setWord(e.target.value)}}
+     />
+     <button
+     type="button"
+     className="outline-none rounded-md px-4 py-2 bg-blue-500 text-white"
+     >
+       Add
+     </button>
     </div>
   );
 }
